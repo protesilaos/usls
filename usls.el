protesilaos/usls

commit 69758ef24cc896330a82b27d0d578166a478e9be

Align negative space of note headers

usls.el
@@ -424,23 +424,23 @@ from there."
       ;; TODO: make those templates somewhat customisable.  We need to
       ;; determine what should be parametrised.
       (".md" `(concat "---" "\n"
-                      "title: " ,title "\n"
-                      "date: " ,date "\n"
-                      "category: " ,cat "\n"
+                      "title:     " ,title "\n"
+                      "date:      " ,date "\n"
+                      "category:  " ,cat "\n"
                       "orig_name: " ,filename "\n"
-                      "orig_id: " ,id "\n"
+                      "orig_id:   " ,id "\n"
                       "---" "\n\n"))
-      (".org" `(concat "#+title: " ,title "\n"
-                       "#+date: " ,date "\n"
-                       "#+category: " ,cat "\n"
+      (".org" `(concat "#+title:     " ,title "\n"
+                       "#+date:      " ,date "\n"
+                       "#+category:  " ,cat "\n"
                        "#+orig_name: " ,filename "\n"
-                       "#+orig_id: " ,id "\n\n"))
-      (_ `(concat "title: " ,title "\n"
-                  "date: " ,date "\n"
-                  "category: " ,cat "\n"
+                       "#+orig_id:   " ,id "\n\n"))
+      (_ `(concat "title:     " ,title "\n"
+                  "date:      " ,date "\n"
+                  "category:  " ,cat "\n"
                   "orig_name: " ,filename "\n"
-                  "orig_id: " ,id "\n"
-                  (make-string 24 ?-) "\n\n")))))
+                  "orig_id:   " ,id "\n"
+                  (make-string 26 ?-) "\n\n")))))
 
 (defun usls--file-region-separator-heading-level (mark str)
   "Format MARK and STR for `usls--file-region-separator-str'.

commit cca8a8d8bb3d78ddfbc9466ec999d3690f682ca2

Fix delimiter fontification caused by 69758ef

usls.el
@@ -911,7 +911,7 @@ directory will be directly displayed instead."
      (1 'usls-header-data-key)
      (2 'usls-header-data-key)
      (3 'usls-header-data-secondary))
-    ("^\\(-\\{24\\}\\|[*\s]\\{5\\}\\)$"
+    ("^\\(-\\{26\\}\\|[*\s]\\{5\\}\\)$"
      (1 'usls-section-delimiter))
     ("\\(\\^\\)\\([0-9_]\\{15\\}\\)"
      (1 'escape-glyph)