protesilaos/usls

commit 1e7bbe19873ad995560feb31143f493eb02778a2

MAJOR BREAKING CHANGE: change category separator
Before we would separate categories with a "-" (single dash), but that
could lead to confusion because the field separator is a "--" (double
dash).  Now the separator is the plus sign.

What follows are instructions to update existing entries.  Apologies for
the inconvenience, as I am not aware of a better way to handle this
case.

* * *

Refactor all your USLS files (WARNING---only tested with Emacs 29):

+ BACK UP ALL YOUR FILES (unless you are already tracking them with git,
  in which case double-check the following changes before committing).

+ Go to the USLS directory (if there are subdirectories, repeat this
  step for each of them).

+ Make Dired editable with `C-x C-q`

+ Start recording a keyboard macro with `F3`, `C-x (` or `M-x kmacro-start-macro`.

+ Type `M-:` and evaluate:

  ```elisp
  (progn
    (goto-char (point-min))
    (if-let* ((end (search-forward-regexp "--\\([0-9A-Za-z_-]*-[0-9A-Za-z_-]*\\)--" nil t))
              (beg (search-backward-regexp "--\\([0-9A-Za-z_-]*-[0-9A-Za-z_-]*\\)--" nil t)))
        (let* ((string (buffer-substring-no-properties beg end))
               (rep (replace-regexp-in-string "-+?" "+" (string-trim string "--" "--"))))
          (delete-region beg end)
          (insert (concat "--" rep "--")))
      (keyboard-quit)))
  ```

+ After evaluating the above, go back to the top of the buffer and close
  the keyboard macro with `F4`, `C-x )` or `M-x kmacro-end-macro`.

+ Repeat the keyboard macro with `C-u 0 F4` (`C-u 0` is the same as
  `C-0`).  If `F4` is not available, use `C-u 0 C-x e`.

+ Confirm the changes with `C-c C-c`.

* * *

Update all references to the original file paths (requires the `wgrep`
package):

+ Type `M-:` `(lgrep "--\\([0-9A-Za-z_-]*-[0-9A-Za-z_-]*\\)--.*\\(txt\\|org\\|md\\)" "*" default-directory)`

+ Instead of `lgrep` you can use `rgrep` if you have subdirectories.

+ In the resulting grep buffer, type `C-x C-q` to make it editable.

usls.el
@@ -178,7 +178,7 @@ the source of this docstring):
 (defconst usls-id-regexp "\\([0-9_]+\\{15\\}\\)"
   "Regular expression to match `usls-id'.")
 
-(defconst usls-category-regexp "\\(--\\)\\([0-9A-Za-z_-]*\\)\\(--\\)"
+(defconst usls-category-regexp "\\(--\\)\\([0-9A-Za-z_+]*\\)\\(--\\)"
   "Regular expression to match `usls-categories'.")
 
 (defconst usls-file-regexp

README
@@ -75,9 +75,12 @@ presentation is compact, with only the underscore separating the two
 components.  Like this: 20201108_091625.  The DATE serves as the unique
 identifier of each note.
 
-CATEGORY is one or more entries separated by a hyphen.  Items that need
-to be more than one word long must be written with an underscore.  So
-"emacs_library" is one category, while "emacs-library" are two.
+CATEGORY is one or more entries separated by a plus sign (the separator
+is inserted automatically when using the command `usls-new-note').
+Items that need to be more than one word long must be written with an
+underscore.  So when "emacs_library" appears in a file name, it is
+interpreted as a single category, while "emacs+library" are two distinct
+categories.
 
 The TITLE is the title of the note that gets extracted and hyphenated.
 An entry about "This is a test" produces a "this-is-a-test" TITLE.
@@ -85,8 +88,9 @@ An entry about "This is a test" produces a "this-is-a-test" TITLE.
 Some complete examples:
 
 20201007_124941--economics--plentiful-and-predictable-liquidity.txt
-20201007_104945--emacs-git--git-patch-formatting.txt
+20201007_104945--emacs+git--git-patch-formatting.txt
 20201105_113805--monetary_policy--asset-bubbles-macroprudential-policy.txt
+20211220_113805--three_word_category+docs+test--this-is-a-test.txt
 
 EXTENSION is one of ".txt", ".md", ".org" and is subject to a
 user-facing customisation option.

commit 8b719064e84b54dace3983d336445b40ae363aa3

Fix usls--inferred-categories regression
This was caused by 1e7bbe1.

usls.el
@@ -363,7 +363,7 @@ trailing hyphen."
   "Extract categories from `usls--directory-files'."
   (let ((sequence (usls--categories-in-files)))
     (mapcan (lambda (s)
-              (split-string s "-" t))
+              (split-string s "+" t))
             sequence)))
 
 (defun usls-categories ()